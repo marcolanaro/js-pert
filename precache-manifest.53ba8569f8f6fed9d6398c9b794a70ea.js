339a4c61153d3ef0f6d0f09c38d1a0dd890a39fd
Marco Lanaro <marcolanaro@gmail.com>
2018-12-17 17:09:35 +0000

    Updates

---
+++ precache-manifest.53ba8569f8f6fed9d6398c9b794a70ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1f86a775e10ddb674a0",
+    "url": "/static/css/main.ed4f41f0.chunk.css"
+  },
+  {
+    "revision": "e1f86a775e10ddb674a0",
+    "url": "/static/js/main.e1f86a77.chunk.js"
+  },
+  {
+    "revision": "33aba93c4d214d0df525",
+    "url": "/static/js/1.33aba93c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e33b1fee7a759467e021d1bfe9cb615e",
+    "url": "/index.html"
+  }
+];
